e2d2dec7967e5b9579b1f7e942e16d3809cb9f4b
ComitialSign <gabrielpalmeiranaza0@gmail.com>
2025-01-26 01:17:51 -0300

    feat: add 2 methods to UserDetails works

--- api/src/main/java/com/comitialsign/api/domain/user/User.java
+++ api/src/main/java/com/comitialsign/api/domain/user/User.java
@@ -41,6 +41,16 @@ public class User implements UserDetails {
     }
 
     @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
     public boolean isAccountNonExpired() {
         return UserDetails.super.isAccountNonExpired();
     }
