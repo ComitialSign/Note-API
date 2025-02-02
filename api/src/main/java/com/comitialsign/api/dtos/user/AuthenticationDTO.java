d0d5f52dee3f19249b69c29428c9e6786f2f664b
ComitialSign <gabrielpalmeiranaza0@gmail.com>
2025-02-02 00:16:00 -0300

    feat: -Changed the name;

--- api/src/main/java/com/comitialsign/api/dtos/user/AuthenticationDTO.java
+++
@@ -1,4 +0,0 @@
-package com.comitialsign.api.dtos.user;
-
-public record AuthenticationDTO(String username, String password) {
-}
---
+++ api/src/main/java/com/comitialsign/api/dtos/user/LoginDTO.java
@@ -0,0 +1,4 @@
+package com.comitialsign.api.dtos.user;
+
+public record LoginDTO(String username, String password) {
+}
