5aa4d79b6a7334fc6ea15ca8e61522dc2ed81f73
ComitialSign <gabrielpalmeiranaza0@gmail.com>
2025-05-15 02:54:10 -0300

    feat: add create and update methods to API.

--- api/src/main/java/com/comitialsign/api/controller/UserController.java
+++ api/src/main/java/com/comitialsign/api/controller/UserController.java
@@ -1,9 +1,36 @@
 package com.comitialsign.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.comitialsign.api.dtos.note.NoteDTO;
+import com.comitialsign.api.service.UserService;
+import jakarta.validation.Valid;
+import jdk.jfr.Description;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
+@Description("User notes controller")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/create")
+    public void createNote(@RequestBody @Valid NoteDTO data) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
+
+        userService.createNote(data, userDetails);
+    }
+
+    @PutMapping("/update/{id}")
+    public void updateNote(@RequestBody @Valid NoteDTO data,@PathVariable UUID id) {
+        userService.updateNote(data, id);
+    }
 }
--- api/src/main/java/com/comitialsign/api/service/UserService.java
+++ api/src/main/java/com/comitialsign/api/service/UserService.java
@@ -1,19 +1,46 @@
 package com.comitialsign.api.service;
 
+import com.comitialsign.api.domain.note.Note;
+import com.comitialsign.api.domain.user.User;
+import com.comitialsign.api.dtos.note.NoteDTO;
 import com.comitialsign.api.repository.NoteRepository;
-import com.comitialsign.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository userRepository;
+    AuthService authService;
 
     @Autowired
     private NoteRepository noteRepository;
 
+    public void createNote(NoteDTO data, UserDetails userDetails){
+        var user = getCurrentUser(userDetails);
+        LocalDateTime now = LocalDateTime.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
 
+        Note note = new Note(data.title(), data.content(), now, now, (User) user);
+        noteRepository.save(note);
+    }
+
+    public void updateNote(NoteDTO data, UUID id){
+        LocalDateTime now = LocalDateTime.now().atZone(ZoneId.systemDefault()).toLocalDateTime();
+
+        noteRepository.findById(id).map(noteUpdated -> {
+            noteUpdated.setTitle(data.title());
+            noteUpdated.setContent(data.content());
+            noteUpdated.setUpdatedAt(now);
+            return noteRepository.save(noteUpdated);
+        }).orElseThrow(() -> new RuntimeException("Nota não encontrada."));
+    }
+
+    private UserDetails getCurrentUser(UserDetails userDetails) {
+        return authService.loadUserByUsername(userDetails.getUsername());
+    }
 }
